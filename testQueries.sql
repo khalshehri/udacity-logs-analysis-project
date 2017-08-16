74202cd6b95167632a7f8eacc91a255c1dd74d9f
Magnus Ringkjøb <magnus@ringkjob.com>
2017-08-16 20:35:15 +0200

    Small fix in 400 log query

--- testQueries.sql
+++ testQueries.sql
@@ -3,8 +3,7 @@
 
 
 -- Finds # logs with error code >= 400:
-SELECT date(time) AS log_date, count(time) AS num_errors,
-  (SELECT count(time) AS num_logs) as lol
+SELECT date(time) AS log_date, count(time) AS num_errors
   FROM log 
   WHERE to_number(substr(status, 1, 3), '999') >= 400
   GROUP BY log_date;
