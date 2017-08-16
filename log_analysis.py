07d4f676d3e7af8a118a0f8b711d1879e842ff94
Magnus Ringkjøb <magnus@ringkjob.com>
2017-08-16 19:53:34 +0200

    Update putput formating

--- log_analysis.py
+++ log_analysis.py
@@ -11,16 +11,22 @@ def query_db(view):
     db.close()
     return posts
 
+def create_headline(text):
+    text = ('** ' + text + ' **').upper()
+    return ('*' * len(text)) + '\n' + text + '\n' + ('*' * len(text))
 
-print('What are the most popular three articles of all time?')
+
+print(create_headline('What are the most popular three articles of all time?'))
 POP_ARTICLES = query_db(view='pop_articles')
 for article in POP_ARTICLES:
     print(article[0] + ' -- ' + str(article[1]) + ' views')
 
-print('\nWho are the most popular article authors of all time?')
+print('\n')
+print(create_headline('Who are the most popular article authors of all time?'))
 POP_AUTHORS = query_db(view='pop_authors')
 for author in POP_AUTHORS:
     print(author[0] + ' -- ' + str(author[1]) + ' views')
 
-print('\nOn which days did more than 1% of requests lead to errors?')
+print('\n')
+print(create_headline('On which days did more than 1% of requests lead to errors?'))
 print('Not yet implemented...')
