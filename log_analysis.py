ef7a40eec585d6a02798f03a3cf0d06c1d547778
Magnus Ringkjøb <magnus@ringkjob.com>
2017-08-16 19:43:39 +0200

    Make code more readable

--- log_analysis.py
+++ log_analysis.py
@@ -12,15 +12,15 @@ def query_db(view):
     return posts
 
 
+print('What are the most popular three articles of all time?')
 POP_ARTICLES = query_db(view='pop_articles')
-
 for article in POP_ARTICLES:
     print(article[0] + ' -- ' + str(article[1]) + ' views')
 
-    print('')
-
-
+print('\nWho are the most popular article authors of all time?')
 POP_AUTHORS = query_db(view='pop_authors')
-
 for author in POP_AUTHORS:
     print(author[0] + ' -- ' + str(author[1]) + ' views')
+
+print('\nOn which days did more than 1% of requests lead to errors?')
+print('Not yet implemented...')
