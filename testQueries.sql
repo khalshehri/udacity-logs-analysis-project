4bac57445e09766db228a77c58b0747b5ca28e50
Magnus Ringkjøb <magnus@ringkjob.com>
2017-08-17 19:42:45 +0200

    backup

--- testQueries.sql
+++ testQueries.sql
@@ -16,12 +16,29 @@ FROM log
 GROUP BY log_date;
 
 -- Not working yet, but we're getting somewhere..
-SELECT log_date, 100. * err.num_logs / normal.num_logs AS err_percent
+SELECT log_date, round((100. * err.num_logs / normal.num_logs)::numeric,1) AS err_percent
 FROM (
     SELECT date(time) AS log_date, count(time) AS num_logs
     from log
     GROUP BY log_date
-) AS normal
+) AS normal 
+LEFT JOIN (
+    SELECT date(time) AS log_date,
+    count(time) AS num_logs
+    FROM log
+    WHERE to_number(substr(status, 1, 3), '999') >= 400
+    GROUP BY log_date
+) AS err USING (log_date);
+
+
+
+-- Almost done
+SELECT log_date, round((100. * err.num_logs / normal.num_logs)::numeric,1) AS err_percent
+FROM (
+    SELECT date(time) AS log_date, count(time) AS num_logs
+    from log
+    GROUP BY log_date
+) AS normal 
 LEFT JOIN (
     SELECT date(time) AS log_date,
     count(time) AS num_logs
